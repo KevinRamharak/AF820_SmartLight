0bcef420cef55795b06dc989c3b8b19c704474bf
Kevin Ramharak <kevinramharak_95@hotmail.com>
2017-11-21 19:37:35 +0100

    Standard skeleton of the packets to send to the AF820_SmartLight

---
+++ src/Protocol/Standard.cpp
@@ -0,0 +1,122 @@
+#include "Standard.h"
+
+namespace AF820_SmartLight {
+    namespace Protocol {
+        Standard::Standard() {}
+
+        void Standard::setLength(const uint8_t length) {
+            this->length[0] = 0x00;
+            this->length[1] = length;
+        }
+        void Standard::setLength(const uint8_t * length) {
+            this->length[0] = length[0];
+            this->length[1] = length[1];
+        }
+        void Standard::setLength(const uint16_t length) {
+            this->length[0] = (uint8_t) (length >> 8);
+            this->length[1] = (uint8_t) length;
+        }
+
+        void Standard::setDeviceMac(const uint8_t * mac) {
+            for(uint8_t i = 0; i < 8; i++) {
+                this->device_mac[i] = mac[i];
+            }
+        }
+        void Standard::setDeviceMac(const uint64_t mac) {
+            // if you forgot how this works unroll the loop
+            for(uint8_t i = 0; i < 8; i++) {
+                this->device_mac[i] = (uint8_t) (mac >> (56 - i * 8) );
+            }
+        }
+
+        void Standard::setDeviceCode(const uint8_t code) {
+            this->device_code = code;
+        }
+
+        void Standard::setDeviceFunction(const uint8_t func) {
+            this->device_function = func;
+        }
+        void Standard::setDeviceFunction(const DeviceFunction func) {
+            this->device_function = (uint8_t) func;
+        }
+
+        void Standard::setControllerMac(const uint8_t * mac) {
+            for(uint8_t i = 0; i < 8; i++) {
+                this->controller_mac[i] = mac[i];
+            }
+        }
+        void Standard::setControllerMac(const uint64_t mac) {
+            // if you forgot how this works unroll the loop
+            for(uint8_t i = 0; i < 8; i++) {
+                this->device_mac[i] = (uint8_t) (mac >> (56 - i * 8) );
+            }
+        }
+
+        void Standard::setSequenceNumber(const uint8_t number) {
+            this->sequence_number[0] = 0x00;
+            this->sequence_number[1] = number;
+        }
+        void Standard::setSequenceNumber(const uint8_t * number) {
+            this->sequence_number[0] = number[0];
+            this->sequence_number[1] = number[1];
+        }
+        void Standard::setSequenceNumber(const uint16_t number) {
+            this->sequence_number[0] = (uint8_t) (number >> 8);
+            this->sequence_number[1] = (uint8_t) number;
+        }
+
+        uint16_t Standard::incrementSequenceNumber() {
+            // https://gitlab.com/iot/af820smartlight#protocol
+            // A monotonically increasing sequence number for each packet.
+            // It is never zero.
+            this->sequence_number[1]++;
+            if(this->sequence_number[1] == 0) {
+                this->sequence_number[0]++;
+                // if [1] wrapped and [0] wrapped they will both be 0 -> since it can never be 0 we increment [1] again
+                if(this->sequence_number[0] == 0) {
+                    this->sequence_number[1] = 1;
+                }
+            }
+
+            return (uint16_t) (this->sequence_number[0] << 8) + this->sequence_number[1];
+        }
+
+        void Standard::setExtraDataLength(const uint8_t length) {
+            this->extra_data_length[0] = 0x00;
+            this->extra_data_length[1] = length;
+        }
+        void Standard::setExtraDataLength(const uint8_t * length) {
+            this->extra_data_length[0] = length[0];
+            this->extra_data_length[1] = length[1];
+        }
+        void Standard::setExtraDataLength(const uint16_t length) {
+            this->extra_data_length[0] = (uint8_t) (length >> 8);
+            this->extra_data_length[1] = (uint8_t) length;
+        }
+
+        uint16_t Standard::copy(uint8_t * buffer, uint16_t buffer_length) {
+            uint16_t stream_length = this->getLength();
+            uint16_t i; // amount of bytes written
+
+            // we use 'getByteStream()' for now this can be replace with a long if/else if condition
+            // but this method should be overidden by child classes
+            const uint8_t * raw_pointer = this->getByteStream();
+
+            for(i = 0; i < stream_length && i < buffer_length; i++) {
+                buffer[i] = raw_pointer[i];
+            }
+
+            return i;
+        }
+
+        const uint8_t * Standard::getByteStream() const {
+            return (uint8_t *) this;
+        }
+        uint16_t Standard::getLength() const {
+            return (uint16_t) (this->length[0] << 8) + this->length[1];
+        }
+        uint16_t Standard::getExtraDataLength() const {
+            return (uint16_t) (this->extra_data_length[0] << 8) + this->extra_data_length[1];
+        }
+    }
+}
---
+++ src/Protocol/Standard.h
@@ -0,0 +1,96 @@
+#ifndef AF820_SMARTLIGHT_PROTOCOL_STANDARD_H_
+#define AF820_SMARTLIGHT_PROTOCOL_STANDARD_H_
+
+#include <cstdint>
+
+#include "DeviceFunction.h"
+
+namespace AF820_SmartLight {
+    namespace Protocol {
+        class Standard {
+        protected:
+            uint8_t header [2] { 0x55, 0xAA };
+            uint8_t length [2] { 0x00, 0x25 };
+            const uint8_t fix_1  [3] { 0x01, 0x00, 0x00 };
+            uint8_t device_mac [8] {};
+            const uint8_t fix_2 [3] {};
+            uint8_t device_code = 0x00;
+            uint8_t device_version = 0x00;
+            uint8_t device_function = 0x00; // this could be of type 'enum struct DeviceFunction'
+            const uint8_t type_big = 0x00;
+            const uint8_t type_small = 0x00;
+            uint8_t controller_mac [4] {};
+            const uint8_t fix_3 [4] {};
+            uint8_t sequence_number [2] { 0x00, 0x01 };
+            uint8_t extra_data_length [2] {};
+            const uint8_t fix_4 [2] {};
+
+            // #NOTE: This is a bit playing with fire since we return a raw byte pointer to the instance in memory.
+            //        We assume its byte layout is its fields/members in the order specified in the header with no padding bytes
+            //        It might be required to pad out the packets ourselfs with zeroed out bytes.
+            const uint8_t * getByteStream() const;
+
+        public:
+            Standard();
+
+            void setLength(const uint8_t length);
+            void setLength(const uint8_t * length);
+            void setLength(const uint16_t length);
+
+            void setDeviceMac(const uint8_t * mac);
+            void setDeviceMac(const uint64_t mac);
+
+            void setDeviceCode(const uint8_t code);
+
+            void setDeviceFunction(const uint8_t func);
+            void setDeviceFunction(const DeviceFunction func);
+
+            void setControllerMac(const uint8_t * mac);
+            void setControllerMac(const uint64_t mac);
+
+            void setSequenceNumber(const uint8_t number);
+            void setSequenceNumber(const uint8_t * number);
+            void setSequenceNumber(const uint16_t number);
+
+            uint16_t incrementSequenceNumber();
+
+            void setExtraDataLength(const uint8_t length);
+            void setExtraDataLength(const uint8_t * length);
+            void setExtraDataLength(const uint16_t length);
+
+            uint16_t copy(uint8_t * buffer, uint16_t buffer_length);// returns amount of bytes copied into the buffer
+            /*
+                Example use of 'copy()':
+                {
+                    // @TODO(kevin@ramahrak.nl) : Maybe use 'std::*_ptr<Protocol::Standard>' or something
+                    Protocol::Standard * p = new Protocol::Standard();
+
+                    // zeroed out tough it should not make a difference when sending UDP packets
+                    // since the data the bulb cares about only reads till specified length
+                    // and the user can use the return value to make sure only the bytes we care about will be send
+                    char stack_buffer[1000] {};
+                    uint16_t bytes_written = p->copy(stack_buffer, 1000); //  @NOTE(kevin@ramharak.nl): does 'stack_buffer' automatically decay to a pointer? or do i need to & it up?
+
+                    // or more dynamic
+                    uint16_t len = p->getLength();
+                    char * heap_buffer = new char[len];
+                    bytes_written = p->copy(heap_buffer, len);
+
+                    // don't forget to delete the instance when you dont need it
+                    delete p;
+
+                    // ... use buffer in program
+
+                    // and delete when done with it
+                    delete[] heap_buffer;
+                }
+
+            */
+
+            uint16_t getLength() const;
+            uint16_t getExtraDataLength() const;
+        };
+    }
+}
+
+#endif// AF820_SMARTLIGHT_PROTOCOL_STANDARD_H_
