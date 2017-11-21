bdca332ea0205b289d004a01ddc1c8a8ada89b22
Kevin Ramharak <kevinramharak_95@hotmail.com>
2017-11-21 19:39:18 +0100

    Usefull enum for DeviceFunction and WifiFunction parts of the protocool

---
+++ src/Protocol/DeviceFunction.h
@@ -0,0 +1,31 @@
+#ifndef AF820_SMARTLIGHT_PROTOCOL_DEVICEFUNCTION_H_
+#define AF820_SMARTLIGHT_PROTOCOL_DEVICEFUNCTION_H_
+
+#include <cstdint>
+
+namespace AF820_SmartLight {
+    namespace Protocol {
+        enum struct DeviceFunction : uint8_t {
+            ON = 0x01,
+            OFF = 0x02,
+            COLOR = 0x03,
+            RANDOM = 0x04,
+            SLEEP = 0x05,
+            WIFI = 0xF0,
+            REGISTER_RESPONSE = 0xFA,
+            REGISTER_REQUEST = 0xFB, // weird remote service that the AF820_SmartLight will inform of everything you send it
+            RESET = 0xF7,
+            RESPONSE = 0xFE,
+            STATUS = 0xFF,
+        };
+
+        enum struct WifiFunction : uint8_t {
+            WIFI_CHECK = 0x01,
+            WIFI_SET = 0x03,
+            RESPONSE = 0x04,
+            WIFI_RESTART = 0x05
+        };
+    }
+}
+
+#endif// AF820_SMARTLIGHT_PROTOCOL_DEVICEFUNCTION_H_
