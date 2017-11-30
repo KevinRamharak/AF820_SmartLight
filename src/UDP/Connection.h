940c44149d244b921eabc191c37a0bf858effab2
Kevin Ramharak <kevinramharak_95@hotmail.com>
2017-11-30 01:23:17 +0100

    UDP connection stubs

---
+++ src/UDP/Connection.h
@@ -0,0 +1,20 @@
+#ifndef AF820_SMARTLIGHT_UDP_CONNECTION_H_
+#define AF820_SMARTLIGHT_UDP_CONNECTION_H_
+
+namespace AF820_SmartLight {
+    namespace UDP {
+        class Connection {
+        private:
+            bool connected = false;
+            int socket_fd = -1;
+
+
+        protected:
+
+        public:
+
+        };
+    }
+}
+
+#endif // AF820_SMARTLIGHT_UDP_CONNECTION_H_
